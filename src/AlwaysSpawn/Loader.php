4bea2e23415ce2478ad4e13f890a8c402118ac1d
StormCraftMCPE <stormcraftmcpe@gmail.com>
2017-07-17 19:02:34 +0700

    Update Loader.php

--- src/AlwaysSpawn/Loader.php
+++ src/AlwaysSpawn/Loader.php
@@ -7,4 +7,18 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\math\Vector3;
 
-class Loader extends Plugin implements Listener
+class Loader extends Plugin implements Listener{
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getServer()->getLogger()->info("AlwaysSpawn Enable!");
+    }
+  
+  public function onPlayerLogin(PlayerLoginEvent $event){
+    $player = $event->getPlayer();
+    $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
+    $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getY();
+    $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
+    $player->setLevel($level);
+    $player->teleport(new Vector3($x, $y, $z, $level
+                                  
