6c34c06efcd0782e83c3d9fc02082a24a4ea53dc
StormCraftMCPE <stormcraftmcpe@gmail.com>
2017-07-17 19:04:56 +0700

    Update Loader.php

--- src/AlwaysSpawn/Loader.php
+++ src/AlwaysSpawn/Loader.php
@@ -14,11 +14,16 @@ class Loader extends Plugin implements Listener{
     $this->getServer()->getLogger()->info("AlwaysSpawn Enable!");
     }
   
+  public function onDisable(){
+    $this->getServer()->getLogger()->info("AlwaysSpawn Disable!");
+    }
+  
   public function onPlayerLogin(PlayerLoginEvent $event){
     $player = $event->getPlayer();
     $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
     $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getY();
     $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
     $player->setLevel($level);
-    $player->teleport(new Vector3($x, $y, $z, $level
-                                  
+    $player->teleport(new Vector3($x, $y, $z, $level));
+    }
+  }
