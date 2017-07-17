e884a4d2b73135ffb0c65fc1f7ba85ced46c6aeb
StormCraftMCPE <stormcraftmcpe@gmail.com>
2017-07-17 18:54:35 +0700

    Create Loader.php

---
+++ src/AlwaysSpawn/Loader.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AlwaysSpawn;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\math\Vector3;
+
+class Loader extends Plugin implements Listener
