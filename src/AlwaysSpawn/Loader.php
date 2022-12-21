d3083d6121f789698a13f280db6ecca2ca6217f7
Vicky <stormcraftmcpe@gmail.com>
2022-12-21 22:39:10 +0700

    Stuck Spawn Checker

--- src/AlwaysSpawn/Loader.php
+++ src/AlwaysSpawn/Loader.php
@@ -1,9 +1,4 @@
 <?php
-/*
-Storm Craft MCPpe
-IP : play.stormcraft.id
-port : 19132
-*/
 namespace AlwaysSpawn;
 
 use pocketmine\plugin\PluginBase;
@@ -22,12 +17,57 @@ class Loader extends Plugin implements Listener{
     $this->getServer()->getLogger()->info("AlwaysSpawn Disable!");
     }
   
-  public function onPlayerLogin(PlayerLoginEvent $event){
-    $player = $event->getPlayer();
-    $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
-    $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getY();
-    $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
-    $player->setLevel($level);
-    $player->teleport(new Vector3($x, $y, $z, $level));
+public function onPlayerLogin(PlayerLoginEvent $event){
+  $player = $event->getPlayer();
+  $level = $this->getServer()->getDefaultLevel();
+  $spawn = $level->getSafeSpawn();
+
+  // Cek Block / object yang menutupi spawn
+  $block = $level->getBlock($spawn);
+  if($block->getId() !== Block::AIR){
+    // Cari Lokasi Spawn
+    $searchPos = $spawn;
+    $foundFreeSpace = false;
+    for($i = 1; $i <= 5; $i++){
+      // Periksa blok di atas lokasi spawn
+      $searchPos->y += 1;
+      $block = $level->getBlock($searchPos);
+      if($block->getId() === Block::AIR){
+        $foundFreeSpace = true;
+        break;
+      }
+      // Periksa blok di sekitar spawn
+      for($j = 0; $j < 4; $j++){
+        $searchPos->y -= 1;
+        switch($j){
+          case 0:
+            $searchPos->x -= 1;
+            break;
+          case 1:
+            $searchPos->z -= 1;
+            break;
+          case 2:
+            $searchPos->x += 1;
+            break;
+          case 3:
+            $searchPos->z += 1;
+            break;
+        }
+        $block = $level->getBlock($searchPos);
+        if($block->getId() === Block::AIR){
+          $foundFreeSpace = true;
+          break 2;
+        }
+      }
     }
+    if(!$foundFreeSpace){
+      // Spawn Player ke Lokasi awal kalo gaada tempat buat spawn
+      $player->sendMessage("[ERROR] Tidak dapat menemukan lokasi yang tersedia di sekitar lokasi spawn, menggunakan lokasi default.");
+      $player->teleport($spawn);
+      return;
+    }
+    $spawn = $searchPos;
   }
+
+  $player->teleport($spawn);
+}
